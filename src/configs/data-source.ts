a061f8ab36f21096bd0aa19db1be9bdae628471b
HyeonTae Jang <gusxo999@gmail.com>
2025-07-10 16:05:23 +0900

    feat/#4/Seed 약간수정

--- src/configs/data-source.ts
+++ src/configs/data-source.ts
@@ -1,4 +1,10 @@
-import { DB_HOST, DB_PASSWORD, DB_PORT, DB_USERNAME } from 'src/lib/constants';
+import {
+  DB_HOST,
+  DB_NAME,
+  DB_PASSWORD,
+  DB_PORT,
+  DB_USERNAME,
+} from 'src/lib/constants';
 import { DataSource } from 'typeorm';
 import { DataSourceOptions } from 'typeorm';
 
@@ -8,7 +14,7 @@ export const typeORMConfig: DataSourceOptions = {
   port: DB_PORT, // DB 포트
   username: DB_USERNAME, // DB 사용자명
   password: DB_PASSWORD, // DB 비밀번호
-  database: process.env.DB_NAME, // DB 이름
+  database: DB_NAME, // DB 이름
   entities: [__dirname + '/../**/*.entity.{js,ts}'], // 엔티티 위치
   synchronize: true, // 자동 스키마 동기화 (개발용)
 };
