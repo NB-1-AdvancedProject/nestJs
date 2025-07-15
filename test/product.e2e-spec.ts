fc0779626626663d274026dddcbc870fb861995e
HyeonTae Jang <gusxo999@gmail.com>
2025-07-15 11:09:15 +0900

    feat/#3/상품목록조회 기본기능2

--- test/product.e2e-spec.ts
+++ test/product.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication, ValidationPipe } from '@nestjs/common';
-import * as request from 'supertest';
+import { INestApplication } from '@nestjs/common';
+import request from 'supertest';
 import { AppModule } from './../src/app.module';
 
 describe('ProductController (e2e)', () => {
