0aada9cfe7658c7ea48d92c786b8517f6ae9f960
hapyvirus <serenity2011@naver.com>
2025-07-17 18:49:10 +0900

    feat: 유저 조회/수정/삭제/찜한 상점 조회

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,44 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Patch,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
 import { UserService } from './user.service';
+import { AuthGuard } from '@nestjs/passport';
+import { UpdateUserDto } from './dto/updateUser.dto';
+import { UserRes } from './dto/userRes.dto';
 
 @Controller('user')
+@UseGuards(AuthGuard('jwt'))
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @Get('/me')
+  getMe(@Request() req): Promise<UserRes> {
+    const userId = req.user.id;
+    return this.userService.userFindId(userId);
+  }
+
+  @Patch('/me')
+  updateMyData(
+    @Request() req,
+    @Body() updateDto: UpdateUserDto,
+  ): Promise<UserRes> {
+    const userId = req.user.id;
+    return this.userService.updateUser(userId, updateDto);
+  }
+
+  @Delete('/me')
+  deletMyData(
+    @Request() req,
+    @Body() body: { password: string },
+  ): Promise<void> {
+    const userId = req.user.id;
+    const { password } = body;
+    return this.userService.deleteUser(userId, password);
+  }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,13 +1,24 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
 import { User } from './user.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { UpdateUserDto } from './dto/updateUser.dto';
+import { Store } from 'src/store/store.entity';
+import { FavoriteStore } from 'src/favorite-store/favorite-store.entity';
+import { UserRes } from './dto/userRes.dto';
 
 @Injectable()
 export class UserService {
   constructor(
     @InjectRepository(User)
     private readonly userRepository: Repository<User>,
+    @InjectRepository(FavoriteStore)
+    private readonly favoriteRepository: Repository<FavoriteStore>,
   ) {}
 
   async userFindId(userId: string) {
@@ -18,6 +29,73 @@ export class UserService {
     if (!userData) {
       throw new NotFoundException('유저를 찾을 수 없습니다.');
     }
-    return userData;
+    return UserService.filterSensitiveUserData(userData);
+  }
+
+  async updateUser(id: string, data: UpdateUserDto): Promise<UserRes> {
+    const { name, password, currentPassword } = data;
+
+    const user = await this.userRepository.findOne({
+      where: { id },
+      relations: ['grade'],
+    });
+
+    if (!user) {
+      throw new NotFoundException('해당 유저를 조회할 수 없습니다.');
+    }
+
+    const isMatch = await bcrypt.compare(currentPassword, user.password);
+    if (!isMatch) {
+      throw new UnauthorizedException('비밀 번호를 다시 확인해주세요');
+    }
+
+    if (password) {
+      const salt = await bcrypt.genSalt();
+      user.password = await bcrypt.hash(password, salt);
+    }
+
+    if (name) {
+      user.name = name;
+    }
+
+    const updateData = await this.userRepository.save(user);
+
+    return UserService.filterSensitiveUserData(updateData);
+  }
+
+  async deleteUser(id: string, password: string): Promise<void> {
+    const user = await this.userRepository.findOne({ where: { id } });
+
+    if (!user) {
+      throw new NotFoundException('해당 유저를 찾을 수 없습니다.');
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      throw new UnauthorizedException('비밀 번호를 다시 확인해주세요.');
+    }
+
+    await this.userRepository.delete(id);
+  }
+
+  async getLikeStore(id: string): Promise<Store[]> {
+    const user = await this.userRepository.findOne({ where: { id } });
+
+    if (!user) {
+      throw new NotFoundException('해당 유저를 찾을 수 없습니다.');
+    }
+
+    const favorites = await this.favoriteRepository.find({
+      where: { user: { id } },
+    });
+
+    const storeList = favorites.map((fav) => fav.store);
+
+    return storeList;
+  }
+
+  static filterSensitiveUserData(user: User) {
+    const { password, ...rest } = user;
+    return rest;
   }
 }
